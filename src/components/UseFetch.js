2390f4d1656b6b74f2986641dccc29563a25159c
nttruong21 <nguyenthetruong100621@gmail.com>
2021-12-25 20:30:07 +0700

    Change timeout of fetch request

--- src/components/UseFetch.js
+++ src/components/UseFetch.js
@@ -25,7 +25,7 @@ const useFetch = (url) => {
           setIsPending(false);
           setError(err.message);
         });
-    }, 1000);
+    }, 100);
   }, [url]);
 
   return { data, isPending, error };
